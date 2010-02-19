db9deae7322a8bbf49fff343a4decff5c8b736f5
Larry Karnowski <larry.karnowski@gmail.com>
2010-02-19 14:16:25 -0500

    WIP: looking for test to demonstrate I'm opening the Faceboxes too quickly   - the contents are being doubled/tripled   - I know the root cause, but I'm looking for a test that truly demonstrates it   - (root cause: facebox.js line #144 should be html() instead of append())

--- spec/javascripts/OpeningSpec.js
+++ spec/javascripts/OpeningSpec.js
@@ -1,12 +1,16 @@
-function waitsForAnimation(f) {
+function waitsUntilFaceboxLoaded(f) {
   waits(100);
   runs(f);
 }
 
 describe("Opening a Facebox", function(){
   afterEach(function(){
+    // waits(50);
+    
     //TODO: actually clear the entire residue of Facebox from the DOM before each test
-    $("#facebox").hide();
+    // runs(function(){
+    //   $("#facebox").hide();
+    // });
   });
   
   describe("to display text", function(){
@@ -29,10 +33,20 @@ describe("Opening a Facebox", function(){
         expect(content.hasClass('on-a-boat')).toBeTruthy();
       });
     });
+    
+    it("correctly clobbers any previous content if opened a second time", function(){
+      $.facebox('first content');
+        var firstContent = $('#facebox .content').text();
+        expect(firstContent).toEqual('first content');
+
+        $.facebox('second content');        
+        var secondContent = $('#facebox .content').text();
+        expect(secondContent).toEqual('second content');
+    });
   });
 
   describe("to display an image", function(){
-    it('add Facebox to the DOM', function(){
+    it('adds Facebox to the DOM', function(){
       $.facebox({ image: 'stairs.jpg' });
       var fb = $('#facebox');
       expect(fb.length).toEqual(1);
@@ -41,7 +55,7 @@ describe("Opening a Facebox", function(){
     it('contains the correct image', function(){
       $.facebox({ image: 'stairs.jpg' });
 
-      waitsForAnimation(function(){
+      waitsUntilFaceboxLoaded(function(){
         var imageSrc = $('#facebox .content img').attr("src");
         expect(imageSrc).toEqual("http://localhost:8888/stairs.jpg");
       });
@@ -54,5 +68,48 @@ describe("Opening a Facebox", function(){
         expect(content.hasClass('on-a-boat')).toBeTruthy();
       });
     });
+  });
+
+  describe("to display the results of an AJAX call", function(){
+    it('adds Facebox to the DOM', function(){
+      $.facebox({ ajax: 'remote.html' });
+      var fb = $('#facebox');
+      expect(fb.length).toEqual(1);
+    });
+    
+    it('contains the correct results', function(){
+      $.facebox({ ajax: 'remote.html' });
+      
+      waitsUntilFaceboxLoaded(function(){
+        var ajaxText = $('#facebox .content').text();
+        expect(ajaxText).toMatch(/^I'm remote\.html, a different file loaded with ajax\.\s*$/);
+      });
+    });
+    
+    // describe("with a style", function(){
+    //   it("adds the given class to the content", function(){
+    //     $.facebox({ image: 'stairs.jpg' }, "on-a-boat");
+    //     var content = $('#facebox .content');
+    //     expect(content.hasClass('on-a-boat')).toBeTruthy();
+    //   });
+    // });
+    
+    // it("correctly clobbers any previous content if opened a second time", function(){
+    //   $.facebox({ ajax: 'remote.html' });
+    //   $.facebox({ ajax: 'remote.html' });
+    // 
+    //   // runs(function(){
+    //   //   var ajaxText = $('#facebox .content').text();
+    //   //   expect(ajaxText).toMatch(/^I'm remote\.html, a different file loaded with ajax\.\s*$/);
+    //   // });
+    //   // 
+    //   waits(100);
+    //     
+    //   runs(function(){
+    //     var ajaxText = $('#facebox .content').text();
+    //     expect(ajaxText).toMatch(/^I'm remote\.html, a different file loaded with ajax\.\s*$/);
+    //   });
+    // });
+    
   });
 });
